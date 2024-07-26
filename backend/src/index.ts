c53698d61803301b7a9c32ca59dfef8b234bd49e
EduardoSilva09 <silvafeitosaeduardo71@gmail.com>
2024-07-25 21:29:24 -0300

    Feat: Mint and Transfer

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv'
 dotenv.config();
 
+import { mintAndTransfer } from './Web3Provider';
+
 import express, { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
 
@@ -9,7 +11,12 @@ const app = express();
 app.use(morgan("tiny"));
 
 app.post("/mint/:wallet", async (req: Request, res: Response, next: NextFunction) => {
-  res.json({})
+  try {
+    const tx = mintAndTransfer(req.params.wallet);
+    res.json(tx);
+  } catch (error) {
+    res.status(500).json(error)
+  }
 })
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
